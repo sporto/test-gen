6bb41745cb589fc211a04f21acccfdc39b03a2e4
Sebastian <s@porto5.com>
2020-09-09 16:39:32 +1000

    Split functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,26 @@
 use std::collections::BTreeMap;
 
-
 pub fn main() {
-    // Some Dhall data
-    let data = "{ x = 1, y = 1 + 1 } : { x: Natural, y: Natural }";
-    // Deserialize it to a Rust type.
-    let deserialized_map: BTreeMap<String, usize> = serde_dhall::from_str(data).parse().unwrap();
+    match read_file() {
+        Ok(data) => {
+            // let deserialized_map: BTreeMap<String, usize> = serde_dhall::from_str(&data).parse().unwrap();
+            // println!("{:?}", deserialized_map)
+            parse_dhall(&data);
+        }
+        Err(_) =>
+            println!("Fail!")
+    };
+}
+
+fn read_file() -> Result<String, String> {
+    Ok("{ x = 1, y = 1 + 1 } : { x: Natural, y: Natural }".to_string())
+}
+
+fn parse_dhall(data: &str) -> Result<String, String> {
+    let deserialized_map: BTreeMap<String, usize> = serde_dhall::from_str(&data)
+        .parse()
+        .map_err(|e| format!("{:?}", e))?;
 
     println!("{:?}", deserialized_map);
-    println!("Hello, world!");
+    Ok("Ok".to_string())
 }
